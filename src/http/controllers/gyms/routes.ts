36cb2fba5a7389d6fb6eb481f7b965a5d88014d0
Renato Machado <renatomachado176@gmail.com>
2025-02-28 21:34:52 +0000

    feat: create-gym route

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from 'fastify'
+
+import { verifyJwt } from '../../middlewares/verify-jwt'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+}
---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,16 @@
+import type { FastifyInstance } from 'fastify'
+
+import { verifyJwt } from '../../middlewares/verify-jwt'
+
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+
+  app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
+}
